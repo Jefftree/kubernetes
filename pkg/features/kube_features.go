c366299671265c6a11b2e6d1592dd9e1218b3171
Jefftree <jeffrey.ying86@live.com>
2026-02-06 15:07:13 -0500

    test dv

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1986,6 +1986,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	genericfeatures.DeclarativeValidationTakeover: {
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	genericfeatures.DetectCacheInconsistency: {
